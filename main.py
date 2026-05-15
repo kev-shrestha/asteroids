cde9d1423c0d9c9dfff1fff2c97215e27ce6cb89
Kevin_Shrestha <Kevinrshrestha@gmail.com>
2026-05-14 22:27:47 -0400

    Replace player with two empty groups: updatable and drawable
    - This is to better organize the code while keeping things separate
    
    - updatable group is for objects that get updated like player position.
    
    - drawable is for objects that are drawn like the player as they move.

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
-    player =  Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-
+    
     Clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player =  Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         log_state()
@@ -24,8 +30,11 @@ def main():
 
         screen.fill("black")
         
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
