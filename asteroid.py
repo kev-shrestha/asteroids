64265a999190923cfaa1cff924b0c43141deb8da
Kevin_Shrestha <Kevinrshrestha@gmail.com>
2026-05-25 13:07:34 -0400

    Add split method and rewrite update method to give asteroids motion
    - split method kills asteroid on hit and if the asteroid is big enough, it will create two smaller asteroids that move in different areas based on a random angle and have faster velocities.
    
    - update method was WRONG. The whole point of the game is to dodge and shoot MOVING asteroids. The position variable changed to += instead of constantly resetting position using =.

--- asteroid.py
+++ asteroid.py
@@ -1,17 +1,36 @@
 from circleshape import CircleShape
 import pygame
-from main import screen
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", (self.x,self.y), self.radius, LINE_WIDTH)
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
 
     def update(self, dt):
-        self.x = (self.velocity * dt)
-        self.y = (self.velocity * dt)
+        self.position += (self.velocity * dt)
 
+
+    def split(self):    
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
         
+        else: 
+            log_event("asteroid_split")
+            new_angle = random.uniform(20, 50)
+            new_asteroid_one_vector = self.velocity.rotate(new_angle)
+            new_asteroid_two_vector = self.velocity.rotate(-new_angle)
+
+            new_smaller_asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
+
+            asteroid_one = Asteroid(self.position.x, self.position.y, new_smaller_asteroid_radius)
+            asteroid_two = Asteroid(self.position.x, self.position.y, new_smaller_asteroid_radius)
+
+            asteroid_one.velocity = pygame.Vector2(new_asteroid_one_vector) * 1.2
+            asteroid_two.velocity = pygame.Vector2(new_asteroid_two_vector) * 1.2
