373fcd19b1ae81db484fba2e2c6a9dfeb790d15a
Kevin_Shrestha <Kevinrshrestha@gmail.com>
2026-05-25 13:08:27 -0400

    Change asteroid.kill() to asteroid.split()

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def main():
             for shot in shots:
                 if asteroid.collides_with(shot) == True:
                     log_event("asteroid_shot")
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
 
